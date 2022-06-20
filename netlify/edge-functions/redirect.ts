61421687039b8b786d483b9705889c3488763d39
Vinicius Bastos <viniciusbastos@Viniciuss-Mac-mini.local>
2022-06-20 16:16:31 -0300

    1.3

---
+++ netlify/edge-functions/redirect.ts
@@ -0,0 +1,16 @@
+import { Context } from "netlify:edge";
+
+export default async (request: Request, context: Context) => {
+
+  const url = new URL(request.url);
+
+  const response = await fetch(
+		'https://sys.vinibot.io/webhook/f2d588b0-133b-4493-a43b-84c9adb6d885?redirect=' + url.pathname,
+		{
+			method: 'GET'
+		}
+	);
+  
+  return response;
+
+};
