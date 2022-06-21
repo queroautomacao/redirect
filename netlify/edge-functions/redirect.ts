42ac1568d797d4b3b1200fc75c6f6d9eb10e5b9f
Vinicius Bastos <viniciusbastos@Viniciuss-Mac-mini.local>
2022-06-21 11:47:09 -0300

    1.6

--- netlify/edge-functions/redirect.ts
+++ netlify/edge-functions/redirect.ts
@@ -3,10 +3,12 @@ import { Context } from "netlify:edge";
 export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
+  const path = url.pathname.replace(/^\/|\/$/g, '');
+  const result = /[\/\.]/gm.test(path);
 
-  if(url.pathname!="/") {
+  if(result === false && path !== "") {
 	const response = await fetch(
-		'https://sys.vinibot.io/webhook/f2d588b0-133b-4493-a43b-84c9adb6d885?redirect=' + url.pathname,
+		'https://sys.vinibot.io/webhook/f2d588b0-133b-4493-a43b-84c9adb6d885?redirect=' + path,
 		{
 			method: 'GET'
 		}
