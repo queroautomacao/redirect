3940022358ef1f52b2c20afdb5d1b07255032c0a
Vinicius Bastos <viniciusbastos@Viniciuss-Mac-mini.local>
2022-07-04 17:01:55 -0300

    2.2

--- netlify/edge-functions/redirect.ts
+++ netlify/edge-functions/redirect.ts
@@ -1,5 +1,4 @@
 import { Context } from "netlify:edge";
-import process from "process";
 
 export default async (request: Request, context: Context) => {
 
@@ -9,7 +8,7 @@ export default async (request: Request, context: Context) => {
   
 	if(result === false && path !== "") {
 
-		const API_ENDPOINT = process.env.WEBHOOK_URL + path;
+		const API_ENDPOINT = Deno.env.get("WEBHOOK_URL") + path;
 		const response = await fetch(API_ENDPOINT);
         const data = await response.json();
 		const link = data.link;
